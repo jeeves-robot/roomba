86278317e84f358aa79f7cd2780f666088c7b7fe
Krittika DSilva <krittika.dsilva@gmail.com>
2016-04-22 15:19:51 -0700

    Code changed!

--- scripts/roomba.py
+++ scripts/roomba.py
@@ -12,35 +12,50 @@ import time
 
 topic = '/cmd_vel_mux/input/navi'
 
+bumped = False
+pub = rospy.Publisher(topic, Twist, queue_size=5)
+
 # This method is called when we hit something.
 # Randomly choose a direction
 def changeDirection(bump):
+    global bumped
     print "Change direction!"
+    bumped = True
 
-    pub = rospy.Publisher(topic, Twist, queue_size=5)
+def rotate():
+    global bumped
     twist = Twist()
 
     # The TA told me to do something like this, but it's not quite working.
     # She basically said to keep calling rotate 10 times for a second or two. 
-    # 
-    if bump.state == BumperEvent.PRESSED:
-        for i in range(50):
-            twist.linear.x = -0.05
-            twist.angular.x = 1
-            pub.publish(twist)
-            time.sleep(0.2)
-        twist.linear.x = 0.05
-        twist.linear.y = 0
-        twist.linear.z = 0
-        twist.angular.x = 0#angle * -1
-        twist.angular.y = 0
-        twist.angular.z = 0
+    for i in range(15):
+        twist.linear.x = -0.05
+        twist.angular.x = 0
         pub.publish(twist)
+        time.sleep(.02)
+    for i in range(50):
+        twist.linear.x = -0.05
+        twist.angular.z = 2
+        pub.publish(twist)
+        time.sleep(.02)
+    pub.publish(twist)
+    bumped = False
+
+def move_forward():
+    twist = Twist()
+    twist.linear.x = 0.05
+    twist.linear.y = 0
+    twist.linear.z = 0
+    twist.angular.x = 0
+    twist.angular.y = 0
+    twist.angular.z = 0
+    pub.publish(twist)
+    time.sleep(0.2)
+
 
 if __name__ == "__main__": 
     print "Starting main"
     rospy.init_node('turtlebot_roomba')
-    pub = rospy.Publisher(topic, Twist, queue_size=5)
  
     # Subscribe to topic /mobile_base/events/bumper
     # to get notifications when we bump into things.
@@ -49,14 +64,10 @@ if __name__ == "__main__":
 
     try: 
         while(1):
-            twist = Twist()
-            twist.linear.x = 0.05
-            twist.linear.y = 0
-            twist.linear.z = 0
-            twist.angular.x = 0
-            twist.angular.y = 0
-            twist.angular.z = 0
-            pub.publish(twist)
-            time.sleep(0.2)
+            if bumped is True:
+                print("Bumped!")
+                rotate()
+            else:
+                move_forward()
     except:
         print "error!"
