099371c5b42e4b0f0a3fe11911e652541b199ac4
Krittika DSilva <krittika.dsilva@gmail.com>
2016-04-22 13:01:37 -0700

    Moves roomba forward, moves back and forth after bump

--- scripts/roomba.py
+++ scripts/roomba.py
@@ -8,6 +8,7 @@ from geometry_msgs.msg import Twist
 from kobuki_msgs.msg import BumperEvent
 from kobuki_msgs.msg import MotorPower
 import random
+import time 
 
 topic = '/cmd_vel_mux/input/navi'
 
@@ -16,13 +17,19 @@ topic = '/cmd_vel_mux/input/navi'
 def changeDirection(bump):
     print "Change direction!"
 
+    pub = rospy.Publisher(topic, Twist, queue_size=5)
+    twist = Twist()
+
+    # The TA told me to do something like this, but it's not quite working.
+    # She basically said to keep calling rotate 10 times for a second or two. 
+    # 
     if bump.state == BumperEvent.PRESSED:
-        pub = rospy.Publisher(topic, Twist, queue_size=5)
-        twist = Twist()
-        angle = random.uniform(-1,1)
-        twist.angular.x = angle 
-        pub.publish(twist)
-        twist.linear.x = 10
+        for i in range(50):
+            twist.linear.x = -0.05
+            twist.angular.x = 1
+            pub.publish(twist)
+            time.sleep(0.2)
+        twist.linear.x = 0.05
         twist.linear.y = 0
         twist.linear.z = 0
         twist.angular.x = 0#angle * -1
@@ -33,26 +40,23 @@ def changeDirection(bump):
 if __name__ == "__main__": 
     print "Starting main"
     rospy.init_node('turtlebot_roomba')
-    pub = rospy.Publisher(topic, Twist, queue_size=5) 
-    
-    motor = rospy.Publisher("/mobile_base/commands/motor_power", MotorPower, queue_size=1)
-    power = MotorPower()
-    power.state = 1
-    motor.publish(power)
+    pub = rospy.Publisher(topic, Twist, queue_size=5)
+ 
     # Subscribe to topic /mobile_base/events/bumper
     # to get notifications when we bump into things.
     sub = rospy.Subscriber('mobile_base/events/bumper', BumperEvent, changeDirection)
     print "Starting while loop"
 
     try: 
-        while(1):	
+        while(1):
             twist = Twist()
-            twist.linear.x = 10 # this number is arbitrary 
+            twist.linear.x = 0.05
             twist.linear.y = 0
             twist.linear.z = 0
             twist.angular.x = 0
             twist.angular.y = 0
             twist.angular.z = 0
             pub.publish(twist)
+            time.sleep(0.2)
     except:
         print "error!"
